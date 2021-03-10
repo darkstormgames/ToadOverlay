5bab9d666f2756db24723e386feac56433af0fd8
darkstormgames <t.eberlein@outlook.de>
2021-03-10 18:57:55 +0100

    Migration to Linux

--- functions/logger.js
+++ functions/logger.js
@@ -2,12 +2,13 @@
  * required modules
  */
 const fs = require('fs');
+const {workingdirectory} = require('../config.json');
 
 module.exports = {
     /**
-    * @param {string} message 
-    * @param {Discord.User} user 
-    * @param {Discord.Guild} guild 
+    * @param {string} message
+    * @param {Discord.User} user
+    * @param {Discord.Guild} guild
     */
     logMessage: (message, user = null, guild = null, channel = null) => {
         let logTime = new Date();
@@ -15,23 +16,35 @@ module.exports = {
             logTime.getFullYear() + '.' +
             (logTime.getMonth()+1) + '.' +
             logTime.getDate() + ' ' +
-            logTime.getHours() + ':' + 
+            logTime.getHours() + ':' +
             logTime.getMinutes() + ':' +
             logTime.getSeconds() + '] ';
-    
+
         if (guild == null && user == null) {
             console.log(datetime + message);
+            fs.appendFile(workingdirectory + 'bot.log', datetime + message + '\n', (err) => {
+                if (err) console.log(err);
+            });
         }
         else if (guild == null && user != null) {
             console.log(datetime + '[USER: ' + user.username + ' (' + user.id + ')] ' + message);
+            fs.appendFile(workingdirectory + 'bot.log', datetime + '[USER: ' + user.username + ' (' + user.id + ')] ' + message + '\n', (err) => {
+                if (err) console.log(err);
+            });
         }
         else if (guild != null && user != null && channel == null) {
             console.log(datetime + '[USER: ' + user.username + ' (' + user.id + ')] [GUILD: ' + guild.name + ' (' + guild.id + ')] ' + message);
+            fs.appendFile(workingdirectory + 'bot.log', datetime + '[USER: ' + user.username + ' (' + user.id + ')] [GUILD: ' + guild.name + ' (' + guild.id + ')] ' + message + '\n', (err) => {
+                if (err) console.log(err);
+            });
         }
         else {
             console.log(datetime + '[USER: ' + user.username + ' (' + user.id + ')] [GUILD: ' + guild.name + ' (' + guild.id + ')] [CHANNEL: ' + channel.name + ' (' + channel.id + ')] ' + message);
+            fs.appendFile(workingdirectory + 'bot.log', datetime + '[USER: ' + user.username + ' (' + user.id + ')] [GUILD: ' + guild.name + ' (' + guild.id + ')] [CHANNEL: ' + channel.name + ' (' + channel.id + ')] ' + message + '\n', (err) => {
+                if (err) console.log(err);
+            });
         }
-    
+
         if (message.toString().includes('Cannot enqueue Handshake after fatal error')) {
             process.exit(4313);
         }
@@ -43,15 +56,21 @@ module.exports = {
             logTime.getFullYear() + '.' +
             (logTime.getMonth()+1) + '.' +
             logTime.getDate() + ' ' +
-            logTime.getHours() + ':' + 
+            logTime.getHours() + ':' +
             logTime.getMinutes() + ':' +
             logTime.getSeconds() + '] ';
-    
+
         if (user != null) {
             console.log(datetime + '[USER: ' + user.username + ' (' + user.id + ')] ' + message);
+            fs.appendFile(workingdirectory + 'bot.log', datetime + '[USER: ' + user.username + ' (' + user.id + ')] ' + message + '\n', (err) => {
+                if (err) console.log(err);
+            });
         }
         else {
             console.log(datetime + message);
+            fs.appendFile(workingdirectory + 'bot.log', datetime + message + '\n', (err) => {
+                if (err) console.log(err);
+            });
         }
 
         if (message.toString().includes('Cannot enqueue Handshake after fatal error')) {
