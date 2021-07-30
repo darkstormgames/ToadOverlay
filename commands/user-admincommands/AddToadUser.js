e8411c4d2e82c8d0e849a636c9559fa887577a24
darkstormgames <t.eberlein@outlook.de>
2021-07-30 03:10:16 +0200

    some more gifs...

--- commands/user-admincommands/AddToadUser.js
+++ commands/user-admincommands/AddToadUser.js
@@ -33,7 +33,27 @@
      */
      execute: (message, args) => {
         if (message.author.id != process.env.ADMIN_ID) {
-            message.channel.send('https://tenor.com/view/youre-not-my-dad-kid-gif-8300190');
+            let rng = Math.floor(Math.random() * 6);
+            switch (rng) {
+                case 0:
+                    message.channel.send('https://tenor.com/view/youre-not-my-dad-kid-gif-8300190');
+                    break;
+                case 1:
+                    message.channel.send('https://tenor.com/view/mst3k-gif-8327808');
+                    break;
+                case 2:
+                    message.channel.send('https://tenor.com/view/youre-not-my-dad-keith-trolls-the-beat-goes-on-you-cant-tell-me-what-to-do-gif-18076704');
+                    break;
+                case 3:
+                    message.channel.send('https://tenor.com/view/youre-not-the-boss-of-me-beca-anna-kendrick-pitch-perfect-dont-tell-me-what-to-do-gif-17475593');
+                    break;
+                case 4:
+                    message.channel.send('https://tenor.com/view/dont-tell-me-what-to-do-gif-4951202');
+                    break;
+                case 5:
+                    message.channel.send('https://tenor.com/view/youre-not-my-supervisor-youre-not-my-boss-gif-12971403');
+                    break;
+            } 
             return;
         }
         base.log.logMessage('Executing command "admin-addtoaduser"', 'admin-addtoaduser', null, message.guild, message.channel, message.author);
