c252d1a780338d1334e2cbfbd5bc097176c3543d
darkstormgames <t.eberlein@outlook.de>
2021-08-18 18:58:23 +0200

    Weeded out more permission errors...

--- Modules/Reactions/ReactionHelper.js
+++ Modules/Reactions/ReactionHelper.js
@@ -8,6 +8,9 @@ module.exports = {
      * @param {Discord.User} user
      */
     RemoveReaction: (reaction, user) => {
+        if(!message.guild.me.permissionsIn(message.channel).has([Discord.Permissions.FLAGS.MANAGE_MESSAGES, Discord.Permissions.FLAGS.READ_MESSAGE_HISTORY])) {
+            return;
+        }
         let userReactions = reaction.message.reactions.cache.filter(reaction => reaction.users.cache.has(user.id));
         try {
             for (let reaction of userReactions.values()) {
