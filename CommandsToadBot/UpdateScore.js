70e96c525c9c789f7a6b910c7349396d1dfa66fa
darkstormgames <darkstormgames@users.noreply.github.com>
2022-06-28 07:12:05 +0200

    Update UpdateScore.js

--- CommandsToadBot/UpdateScore.js
+++ CommandsToadBot/UpdateScore.js
@@ -12,8 +12,8 @@ module.exports = {
     * @param {Discord.Message} message
     */
     execute: (message) => {
-        let currentHome = message.embeds[0].fields[0].value;
-        let currentGuest = message.embeds[0].fields[1].value;
+        let currentHome = message.embeds[1].fields[0].value;
+        let currentGuest = message.embeds[1].fields[1].value;
         Data.ChannelData.UpdateScores(currentHome, currentGuest, message.channel.id, (error) => {
             message.channel.send('There was an error updating war data...\nPlease try again later... Or not, because it´s already too late...');
             Log.logMessage('There was an error updating war data...', 'update-result', error, message.guild, message.channel, message.author);
